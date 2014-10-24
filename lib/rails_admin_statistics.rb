a593f0443587431795858c552365046610e91906
Konstantin K <konstantin.koss@gmail.com>
2014-10-24 17:26:32 +0200

    hopefully use precompiled assets now on production

--- lib/rails_admin_statistics.rb
+++ lib/rails_admin_statistics.rb
@@ -76,4 +76,3 @@ module RailsAdmin
     end
   end
 end
-
--- lib/rails_admin_statistics/engine.rb
+++ lib/rails_admin_statistics/engine.rb
@@ -1,4 +1,12 @@
 module RailsAdminStatistics
   class Engine < ::Rails::Engine
+    initializer 'RailsAdminStatistics precompile hook', group: :all do |app|
+      app.config.assets.precompile += %w(
+        jquery.flot.js
+        jquery.flot.resize.js
+        rails_admin_statistics.js
+        rails_admin_statistics.css
+      )
+    end
   end
 end
